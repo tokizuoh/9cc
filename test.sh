e5d1160dd7a20f2f5e486c8b63dc9957b2e6501e
tokizuoh <tokizuoh@gmail.com>
2023-05-27 15:55:28 +0900

    step3: introduce tokenizer

--- test.sh
+++ test.sh
@@ -18,14 +18,21 @@ assert() {
 }
 
 # ステップ1: 整数1個をコンパイルする言語の作成
+echo "- step 1"
 assert 0 0
 assert 42 42
+echo ""
 
 # ステップ2: 加減算のできるコンパイラの作成
+echo "- step 2"
 assert 21 "5+20-4"
 assert 6 "7-1"
+echo ""
 
 # ステップ3：トークナイザを導入
-# assert 41 " 12 + 34 - 5 "
+echo "- step 3"
+assert 41 " 12 + 34 - 5 "
+assert 20 "8+2-45+55"
+echo ""
 
 echo OK
